0abe8becee1d00ed73c69bfc597d8951a5501a8b
Loren Colcol <lcolcol@ucsc.edu>
2015-01-10 01:55:41 -0800

    update to my team
    working on a myTeam page

---
+++ TeamFit/Team.swift
@@ -0,0 +1,14 @@
+//
+//  Team.swift
+//  TeamFit
+//
+//  Created by Loren Colcol on 1/10/15.
+//  Copyright (c) 2015 TeamFit. All rights reserved.
+//
+
+import Foundation
+
+struct myTeam {
+    let name : String
+    let goal : Int
+}
---
+++ TeamFit/TeamTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TeamTableViewCell.swift
+//  TeamFit
+//
+//  Created by Loren Colcol on 1/10/15.
+//  Copyright (c) 2015 TeamFit. All rights reserved.
+//
+
+import UIKit
+
+class TeamTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
